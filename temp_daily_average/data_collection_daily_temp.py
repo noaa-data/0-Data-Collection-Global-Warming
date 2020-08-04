e03ceb898c639de0ead4095d3fd92e9370c37230
darrida <darrida.py@gmail.com>
2020-08-04 05:07:38 +0100

    revised for data_dir parameter, registered with prefect agent, running

--- temp_daily_average/data_collection_daily_temp.py
+++ temp_daily_average/data_collection_daily_temp.py
@@ -1,3 +1,5 @@
+#!/home/pi/github/Data-Collection-Global-Warming/venv/bin python3
+
 from prefect import task, Flow, Parameter
 from prefect.schedules import IntervalSchedule
 from prefect.engine.signals import SKIP
@@ -15,8 +17,9 @@ import requests
 
 
 @task # pylink: disable=
-def find_highest_year(url: str):
-    year_folders = os.listdir(path=Path('data'))
+def find_highest_year(url: str, data_dir):
+    #data_dir = Path.home() / 'data_downloads' / 'noaa_daily_avg_temps'
+    year_folders = os.listdir(path=data_dir)
     if year_folders:
         return max(year_folders)
     else:
@@ -43,16 +46,17 @@ def query_cloud_csvs(url: str, year: int) -> set:
 
 
 @task
-def query_local_csvs(year: int) -> set:
+def query_local_csvs(year: int, data_dir: str) -> set:
     csv_local_set = set()
-    csv_folder = Path(Path.cwd() / 'data' / str(year)).rglob('*.csv')
+    data_dir = Path(data_dir)
+    csv_folder = (data_dir / str(year)).rglob('*.csv') #Path(Path.cwd() / 'data' / str(year)).rglob('*.csv')
     csv_local_list = [x for x in csv_folder]
     for i in csv_local_list:
         csv_local_set.add(str(i).split('/')[-1])
     return csv_local_set
 
 
-@task
+@task(log_stdout=True)
 def query_diff_local_cloud(local_set: set, cloud_set: set) -> set:
     diff_set = cloud_set.difference(local_set)
     if diff_set:
@@ -62,11 +66,12 @@ def query_diff_local_cloud(local_set: set, cloud_set: set) -> set:
     return diff_set
 
 
-@task
-def download_new_csvs(url: str, year: int, diff_set: set) -> bool:
+@task(log_stdout=True)
+def download_new_csvs(url: str, year: int, diff_set: set, data_dir: str) -> bool:
     if int(year) > 0:
         count = 0
-        download_path = Path('data') / str(year)
+        data_dir = Path(data_dir)
+        download_path = data_dir / str(year) #Path('data') / str(year)
         if os.path.exists(download_path) == False:
             Path(download_path).mkdir(parents=True, exist_ok=True)
 
@@ -76,7 +81,8 @@ def download_new_csvs(url: str, year: int, diff_set: set) -> bool:
                     download_url = url + '/' + i
                     print(download_url)
                     result = requests.get(download_url)
-                    open(f'data/{year}/{i}', 'wb').write(result.content)
+                    file_path = Path(data_dir / year / i)
+                    open(file_path, 'wb').write(result.content)
                     #print(result.content)
                 except requests.exceptions.InvalidURL:
                     print('Bad url', i)
@@ -87,8 +93,8 @@ def download_new_csvs(url: str, year: int, diff_set: set) -> bool:
         return True
 
 
-@task
-def find_new_year(next_year: bool, year: int):
+@task(log_stdout=True)
+def find_new_year(next_year: bool, year: int, data_dir: str):
     if next_year:
         url = 'https://www.ncei.noaa.gov/data/global-summary-of-the-day/access'
         response = requests.get(url)
@@ -102,7 +108,8 @@ def find_new_year(next_year: bool, year: int):
         for i in cloud_year_set:
             if int(i) > int(year):
                 year = i
-                download_path = Path('data') / str(year)
+                data_dir = Path(data_dir)
+                download_path = data_dir / str(year) #Path('data') / str(year)
                 if os.path.exists(download_path) == False:
                     Path(download_path).mkdir(parents=True, exist_ok=True)
                 print('STATUS => new year:', year)
@@ -110,19 +117,22 @@ def find_new_year(next_year: bool, year: int):
     print('STATUS => current year not finished.')
 
 
-schedule = IntervalSchedule(interval=timedelta(minutes=0.2))
+schedule = IntervalSchedule(interval=timedelta(minutes=30))
 
 
 with Flow('NOAA Daily Average Temp Records', schedule) as flow:
     base_url = Parameter('base_url', default='https://www.ncei.noaa.gov/data/global-summary-of-the-day/access/')
-    
-    t1_year = find_highest_year(url=base_url)
+    data_dir = Parameter('data_dir', default=str(Path.home() / 'data_downloads' / 'noaa_daily_avg_temps'))
+
+    t1_year = find_highest_year(url=base_url, data_dir=data_dir)
     t2_url  = build_url(year=t1_year)
     t3_cset = query_cloud_csvs(url=t2_url, year=t1_year)
-    t4_lset = query_local_csvs(t1_year)
+    t4_lset = query_local_csvs(year=t1_year, data_dir=data_dir)
     t5_dset = query_diff_local_cloud(local_set=t4_lset, cloud_set=t3_cset)
-    t6_next = download_new_csvs(url=t2_url, year=t1_year, diff_set=t5_dset)
-    t7_task = find_new_year(next_year=t6_next, year=t1_year)
+    t6_next = download_new_csvs(url=t2_url, year=t1_year, diff_set=t5_dset, data_dir=data_dir)
+    #t7_task = 
+    find_new_year(next_year=t6_next, year=t1_year, data_dir=data_dir)
 
 
-flow.run()
+#flow.run()
+flow.register(project_name="Global Warming Data")
